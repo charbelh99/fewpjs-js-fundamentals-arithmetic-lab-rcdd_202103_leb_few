db22fc7b11571c55fb00218d50000059e947f031
Charbel Hbayter <charbel.hbayter@lau.edu>
2021-04-07 11:40:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,9 +2,13 @@
 // Write your code here
 let num1 = 6.2;
 let num2 = 10;
-var multiply = num1*num2
+var multiply = Math.floor(num1*num2);
 
 
 let random=Math.floor(Math.random()*10)+1;
 
-let mod
+let num3=19;
+let num4=5;
+var mod =num3%num4;
+
+var max=Math.max(1,2,4,20);
